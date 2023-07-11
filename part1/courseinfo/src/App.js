9735d4b2099ccb219025a329563b4df94d8e9df0
Jeffrey Willis <jeffrey@Jeffreys-Laptop.local>
2023-07-11 18:44:07 +0200

    Add exercise 2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -11,15 +11,9 @@ const Content = (props) => {
   console.log(props);
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <p>{props.parts[0].part} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].part} {props.parts[1].exercises}</p>
+      <p>{props.parts[2].part} {props.parts[2].exercises}</p>
     </div>
   )
 }
@@ -42,10 +36,16 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  const parts = [
+    { part: 'Fundamentals of React', exercises: 10 },
+    { part: 'Using props to pass data', exercises: 7 },
+    { part: 'State of a component', exercises: 14 }
+  ]
+
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content parts={parts}/>
       <Total part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
