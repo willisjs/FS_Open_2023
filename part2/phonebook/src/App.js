0a0c33a5bd8950b0bf19b6a1db1ddf1cb8f92663
Jeffrey Willis <jeffrey@Jeffreys-Laptop.local>
2023-07-26 13:02:06 +0200

    Add Ex2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'George P. Burdell', id: 0}
+  ]) 
+  
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObj = {
+      name: newName,
+      id: persons.length,
+    }
+  
+    setPersons(persons.concat(nameObj));
+    setNewName('');
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value = {newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <ul key={person.id}>{person.name}</ul>)}
+    </div>
+  )
+}
+
+export default App;
