d039df9b0cba43cf6a6a89f769af2e8e7ee756ca
Jeffrey Willis <jeffrey@Jeffreys-Laptop.local>
2023-07-26 13:18:38 +0200

    Add Ex2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,28 +2,37 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'George P. Burdell', id: 0}
+    { name: 'George P. Burdell', id: 1, phone: '(911) 123-4567'}
   ]) 
   
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   }
 
+  const handlePhoneChange = (event) => {
+    console.log(event.target.value);
+    setNewPhone(event.target.value);
+  }
+
   const addName = (event) => {
-    event.preventDefault()
+    event.preventDefault();
+
     const nameObj = {
       name: newName,
-      id: persons.length,
-    }
+      id: persons.length + 1,
+      phone: newPhone
+    };
 
     if (persons.some(person => person.name === newName)) {
       window.alert(`${newName} is already in the database.`);
     } else {
       setPersons(persons.concat(nameObj));
       setNewName('');
+      setNewPhone('');
     } 
   }
 
@@ -35,11 +44,14 @@ const App = () => {
           name: <input value = {newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value = {newPhone} onChange={handlePhoneChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <ul key={person.id}>{person.name}</ul>)}
+      {persons.map(person => <ul key={person.id}>{person.name} {person.phone}</ul>)}
     </div>
   )
 }
